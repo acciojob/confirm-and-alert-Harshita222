cf7c25fbaaa16cc126e1cfcb1744f31cbdcfffbf
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-04-03 19:10:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//your JS code here. If required.
-function askConfirm(){
-	var response =  window.confirm("Do you want to proceed?”);
-		if (response == true) {
-			window.alert("You  clicked OK. Proceeding...")	;
-		return true
-		} else{
-      window.alert("You clicked Cancel. Exiting...");
-		return false;
-		}
+function askConfirm() {
+    var response = window.confirm("Do you want to proceed?");
+    if (response) {
+        window.alert("You clicked OK. Proceeding...");
+        return true;
+    } else {
+        window.alert("You clicked Cancel. Exiting...");
+        return false;
+    }
 }
